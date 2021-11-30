9f34990dd06f4409828ba46db5a0ebb90ea1da14
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-11-30 17:06:04 +0100

    Fix import statement

---
+++ components/organisms/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Logo } from './Navbar.styles';
+import Navigation from '../../molecules/Navigation/Navigation';
+
+const Navbar: React.FC = () => (
+  <nav className="flex fixed inset-0 w-screen h-24 bg-amber-default items-center justify-between shadow-md">
+    <Logo>schoola</Logo>
+    <Navigation />
+  </nav>
+);
+
+export default Navbar;
