b31f07a16db5b98406fc670c18fb9f9c69e146df
Teodor Wolski <teodorwolski88@gmail.com>
2021-11-30 16:46:31 +0100

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import Navbar from '../components/Navbar/Navbar';
 import SectionCTA from '../components/SectionCTA/SectionCTA';
 
-<<<<<<< HEAD
 const Home: NextPage = () => (
   <div>
     <Head>
@@ -14,24 +13,9 @@ const Home: NextPage = () => (
     </Head>
     <div>
       <Navbar />
-      {/* <SectionCTA /> */}
+      <SectionCTA />
     </div>
   </div>
 );
 
-=======
-const Home: NextPage = () => {
-  return (
-    <div className="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
-      <Head>
-        <title>Schoola Landing page</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet" />
-      </Head>
-      <h1 className="text-2xl">hello</h1>
-    </div>
-  );
-};
->>>>>>> eaed84719397f07a2c0bcb21ced8accdffafb1d5
 export default Home;
