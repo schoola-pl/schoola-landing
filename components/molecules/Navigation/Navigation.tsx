4a91c794b8face3f765f9b0c41c27cc453726820
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-02 18:19:29 +0100

    Add better navigation component to NavBar

--- components/molecules/Navigation/Navigation.tsx
+++ components/molecules/Navigation/Navigation.tsx
@@ -12,7 +12,7 @@ const Navigation: React.FC = () => {
     <nav>
       <ul className="flex">
         {routes.map((route) => (
-          <li key={route.name} className="pr-10 pl-10 mr-10 pt-5 pb-5 transition-colors text-xl cursor-pointer text-white hover:text-emerald-dark">
+          <li key={route.name} className="pr-10 pl-10 mr-10 pt-5 pb-5 transition-colors text-xl cursor-pointer hover:text-emerald-dark">
             <Link href={route.path}>{route.name}</Link>
           </li>
         ))}
