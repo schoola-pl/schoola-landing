54f0fb24699a5a8e6d01905d8f03e2103ac3e0e2
Teodor Wolski <teodorwolski88@gmail.com>
2021-12-03 15:00:05 +0100

    Changing favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,6 +38,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.png" />
         </Head>
         <body>
           <Main />
