f7d9c6c2d955d2607f8647c75053cd6261285956
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-03 16:55:13 +0100

    Edit styles of error sites

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,17 +5,17 @@ import Link from 'next/link';
 
 const Err404 = () => {
   return (
-    <div className="h-auto pt-10 md:h-screen w-screen bg-gray-100 flex items-center justify-center">
+    <div className="h-auto pt-10 md:pt-0 w-screen bg-gray-100 flex items-center justify-center" style={{ height: 600 }}>
       <div className="container flex flex-col-reverse md:flex-row items-center justify-center px-5 text-gray-700">
         <div className="max-w-md mt-10 md:mt-0">
           <div className="text-7xl font-dark font-bold">404</div>
           <p className="text-2xl md:text-3xl font-light leading-normal">Nie możemy tego znaleźć...</p>
-          <p className="mb-8 opacity-90 mt-2">Ale nie martw się, na naszej stronie jest masa innych rzeczy do zwiedzania. Śmiało!</p>
+          <p className="mb-5 opacity-90 mt-2 md:mb-8">Ale nie martw się, na naszej stronie jest masa innych rzeczy do zwiedzania. Śmiało!</p>
 
           <Link href={'/'}>
             <button
-              className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-emerald-dark
-           active:bg-emerald-darker hover:bg-emerald-darker"
+              className="px-10 inline py-6 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-emerald-dark
+           active:bg-emerald-darker hover:bg-emerald-darker text-xl md:px-6 md:py-4"
             >
               No i do domku!
             </button>
--- pages/500.tsx
+++ pages/500.tsx
@@ -8,16 +8,18 @@ const reloadSite = () => {
 
 const Err404 = () => {
   return (
-    <div className="h-auto pt-10 md:h-screen w-screen bg-gray-100 flex items-center justify-center">
+    <div className="h-auto pt-10 md:pt-0 w-screen bg-gray-100 flex items-center justify-center" style={{ height: 600 }}>
       <div className="container flex flex-col-reverse md:flex-row items-center justify-center px-5 text-gray-700">
         <div className="max-w-md mt-10 md:mt-0">
           <div className="text-7xl font-dark font-bold">500</div>
-          <p className="text-2xl md:text-3xl font-light leading-normal">Przepraszamy, to nasza wina...</p>
-          <p className="mb-8 opacity-90 mt-2">Coś się nam lekko zepsuło, lecz nie martw się, zaraz to naprawimy!</p>
+          <p className="text-2xl md:text-3xl font-light leading-normal">Coś popsuliśmy!</p>
+          <p className="mb-5 opacity-90 mt-2 md:mb-8">
+            Każdemu zdarzają się błędy, ale prosimy o cierpliwość! Właśnie pracujemy nad naprawieniem usterki!
+          </p>
 
           <button
-            className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-emerald-dark
-           active:bg-emerald-darker hover:bg-emerald-darker"
+            className="px-10 inline py-6 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-emerald-dark
+           active:bg-emerald-darker hover:bg-emerald-darker text-xl md:px-6 md:py-4"
             onClick={reloadSite}
           >
             Odśwież stronę
