454e138017d65c342e4455c2cb3fbfa9de125e73
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-04 18:29:10 +0100

    Add footer to application

---
+++ components/organisms/Footer/Footer.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const Footer = () => {
+  const { push } = useRouter();
+  const goToApplication = () => {
+    push('https://app.schoola.pl/');
+  };
+
+  return (
+    <footer className="bg-emerald-dark">
+      <div className="max-w-2xl mx-auto text-white py-10">
+        <div className="text-center">
+          <h3 className="text-3xl mb-3">Pobierz naszą aplikację szkolną</h3>
+          <p>Poznaj ludzi. Otwórz się, ucz się.</p>
+        </div>
+        <div className={'flex items-center h-36 justify-center mb-5 mt-1'}>
+          <button
+            onClick={goToApplication}
+            className={'rounded-2xl py-4 px-9 border-2 hover:bg-amber-medium transition-colors hover:text-emerald-dark'}
+          >
+            Zacznijmy!
+          </button>
+        </div>
+        <div className="flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
+          <p className="order-2 md:order-1 mt-8 md:mt-0">&copy; Schoola, 2021-2022</p>
+          <div className="order-1 md:order-2">
+            <nav>
+              <Link href={'/about'}>
+                <span className="px-2 cursor-pointer transition-opacity opacity-80  hover:opacity-100">O nas</span>
+              </Link>
+              <Link href={'/app'}>
+                <span className="px-2 border-l cursor-pointer transition-opacity opacity-80  hover:opacity-100">Aplikacja</span>
+              </Link>
+              <Link href={'/contact'}>
+                <span className="px-2 border-l cursor-pointer transition-opacity opacity-80  hover:opacity-100">Kontakt</span>
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/templates/Layout.tsx
+++ components/templates/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from 'components/organisms/Navbar/Navbar';
+import Footer from '../organisms/Footer/Footer';
 
 const Layout: React.FC = ({ children }) => {
   return (
@@ -8,7 +9,7 @@ const Layout: React.FC = ({ children }) => {
       <main aria-live={'polite'} aria-atomic={true}>
         {children}
       </main>
-      {/*<Footer/>*/}
+      <Footer />
     </>
   );
 };
