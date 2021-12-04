4efb09aa65c02a9632e10b44d0d79cc68dca4856
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-04 14:21:26 +0100

    Remove styled-components from project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,6 @@
-import Document, { DocumentContext, DocumentProps, Head, Html, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  constructor(props: DocumentProps, context: any) {
-    super(props, context);
-  }
-
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-          enhanceComponent: (Component) => Component
-        });
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
   render() {
     return (
       <Html lang="pl">
@@ -38,7 +9,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-          <link rel="shortcut icon" href="/favicon.png" />
+          <link rel="shortcut icon" href="favicon.png" />
         </Head>
         <body>
           <Main />
