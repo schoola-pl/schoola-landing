f8f0383201db5c990b1f57831361615c162526c9
Teodor Wolski <teodorwolski88@gmail.com>
2021-12-04 13:24:11 +0100

    Fixing positioning

--- components/organisms/SectionCTA/SectionCTA.tsx
+++ components/organisms/SectionCTA/SectionCTA.tsx
@@ -2,9 +2,9 @@ import ConnectIcon from 'assets/icons/ConnectIcon.svg';
 import Image from 'next/image';
 
 const SectionCTA: React.FC = () => (
-  <div className="grid grid-cols-2">
-    <div className="relative top-52 left-12 col-span-1">
-      <div className="flex flex-col items-start py-6 px-4">
+  <div className="grid left-12 relative top-16  grid-cols-2">
+    <div className="col-span-1">
+      <div className="flex flex-col items-start py-10 px-4">
         <h1 className="text-7xl py-6 font-extrabold tracking-tight">Lorem ipsum dolor sit amet</h1>
         <p className="text-2xl py-10 max-w-3xl">
           lorem ipsum dolor sit am lorem Lorem ipsum dolor sit Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nihil omnis optio
@@ -25,7 +25,7 @@ const SectionCTA: React.FC = () => (
       </div>
     </div>
     <div>
-      <div className="relative top-20 ml-20">
+      <div className="relative bottom-32 ml-20">
         <Image src={ConnectIcon} width={600} height={600} alt={'Connect people icon'} />
       </div>
     </div>
