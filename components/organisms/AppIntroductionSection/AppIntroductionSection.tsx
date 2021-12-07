c5985fe54d479c6f12ee8b501ab0869b9d500fce
Teodor Wolski <teodorwolski88@gmail.com>
2021-12-07 21:12:27 +0100

    Fixing padding on CTA

--- components/organisms/AppIntroductionSection/AppIntroductionSection.tsx
+++ components/organisms/AppIntroductionSection/AppIntroductionSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AppIntroductionSection = () => (
-  <div className="bg-amber-default relative mb-10 p-12 h-full flex flex-col justify-center text-center 2xl:p-28">
+  <div className="bg-amber-default relative mb-10 p-12 h-full flex flex-col justify-center text-center">
     <h1 className="font-bold text-3xl pt-6 pb-4 text-gray-900 sm:text-4xl md:pb-16 2xl:text-5xl">
       Cała szkoła w jednej aplikacji<span className="text-emerald-dark">.</span>
     </h1>
--- components/organisms/SectionCTA/SectionCTA.tsx
+++ components/organisms/SectionCTA/SectionCTA.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 import React from 'react';
 
 const SectionCTA: React.FC = () => (
-  <div className="h-full flex items-center justify-center flex-col-reverse md:flex-row px-5 md:px-3 xl:px-16 xl:py-0 2xl:mb-10 2xl:py-32">
-    <div className="flex flex-col items-start py-10 px-4 md:py-20 lg:py-26 ">
+  <div className="h-full flex items-center justify-center flex-col-reverse md:flex-row px-5 md:px-3 xl:px-16 xl:py-0 2xl:mb-4 2xl:py-20">
+    <div className="flex flex-col items-start py-10 px-4 md:py-20 lg:py-26  ">
       <h1 className="text-5xl md:text-6xl mb-8 font-extrabold tracking-tight xl:text-7xl 2xl:mb-20">Lorem ipsum dolor sit amet</h1>
       <p className="text-lg md:text-xl 2xl:text-2xl max-w-3xl text-justify md:text-left">
         lorem ipsum dolor sit am lorem Lorem ipsum dolor sit Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nihil omnis optio
@@ -30,29 +30,3 @@ const SectionCTA: React.FC = () => (
 );
 
 export default SectionCTA;
-
-//xl
-// <div className="h-full flex items-center justify-center flex-col-reverse md:flex-row px-5 md:px-3">
-//   <div className="flex flex-col items-start py-10 px-4">
-//     <h1 className="text-7xl mb-8 font-extrabold tracking-tight">Lorem ipsum dolor sit amet</h1>
-//     <p className="text-2xl max-w-3xl">
-//       lorem ipsum dolor sit am lorem Lorem ipsum dolor sit Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nihil omnis optio
-//       cupiditate ullam cupiditate ullam cupiditate ullam cupiditate ullam
-//     </p>
-//     <div className="mt-10  flex content-start">
-//       <div className="inline-flex rounded-md shadow">
-//         <button className="inline-flex items-center justify-center text-xl px-20 py-5 border border-transparent font-medium rounded-md text-emerald-dark hover:bg-emerald-dark transition-all hover:text-coolGray-white hover:border-emerald-dark">
-//           Get started
-//         </button>
-//       </div>
-//       <div className="ml-5 inline-flex rounded-md shadow">
-//         <button className="inline-flex text-coolGray-white relative items-center justify-center text-xl px-20 py-5 font-medium rounded-md bg-blue-default hover:bg-blue-light  transition-all">
-//           Learn more
-//         </button>
-//       </div>
-//     </div>
-//   </div>
-//   <div className="md:ml-10">
-//     <Image src={ConnectIcon} width={600} height={600} alt={'Connect people icon'} />
-//   </div>
-// </div>;
