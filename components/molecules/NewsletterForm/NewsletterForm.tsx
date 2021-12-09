664c1178208b191a7292ce7810cc5527d0d884a9
Teodor Wolski <teodorwolski88@gmail.com>
2021-12-09 17:24:01 +0100

    Mailchimp works, types error

--- components/molecules/NewsletterForm/NewsletterForm.tsx
+++ components/molecules/NewsletterForm/NewsletterForm.tsx
@@ -1,26 +1,25 @@
 import { useState, useRef } from 'react';
 
-const NewsletterForm = ({ onValidated: any }) => {
-  const [email, setEmail] = useState(null);
+const NewsletterForm: React.FC = ({ onValidated }) => {
+  const [email, setEmail] = useState('');
   const emailRef = useRef<HTMLInputElement>(null);
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setEmail(emailRef.current.value);
+    setEmail(emailRef.current?.value);
     const formEmail = onValidated({ EMAIL: email });
-    console.log(formEmail);
     return formEmail;
   };
 
   return (
     <div className="mb-10 flex flex-col">
       <h1 className="text-center font-semibold mb-2 text-lg">Zapisz się na nasz newsletter!</h1>
-      <form onSubmit={handleFormSubmit} className="flex justify-center ">
+      <form onSubmit={handleFormSubmit} className="flex justify-center w-30 mx-2">
         <input
           ref={emailRef}
           type="email"
           placeholder="email"
-          className="font-medium w-30 px-2 py-5 mr-2 rounded-md border-2 border-emerald-dark transition-all"
+          className="font-medium max-w-full px-2 py-5 mr-2 rounded-md border-2 border-emerald-dark transition-all"
         />
         <button
           className="font-medium px-10 py-5 rounded-md bg-blue-default transition-all text-coolGray-white hover:border-coolGray-white"
