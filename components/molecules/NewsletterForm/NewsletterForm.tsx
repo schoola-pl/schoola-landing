06fed8971f108e898e2b50de97d787d6fdee4d96
Teodor Wolski <teodorwolski88@gmail.com>
2021-12-10 12:18:07 +0100

    Responsive newsletter section

--- components/molecules/NewsletterForm/NewsletterForm.tsx
+++ components/molecules/NewsletterForm/NewsletterForm.tsx
@@ -17,18 +17,22 @@ const NewsletterForm: React.FC<props> = ({ onValidated }) => {
   };
 
   return (
-    <div className="mb-10 flex flex-col">
-      <h1 className="text-center font-semibold mb-3 text-xl">Zapisz się na nasz newsletter!</h1>
-      <form onSubmit={handleFormSubmit} className="flex justify-center w-30 mx-2">
-        <div className="w-30">
+    <div className="mb-10 flex flex-col md:my-20">
+      <h1 className="text-center font-semibold mb-3 text-xl md:text-3xl md:mb-5">Zapisz się na nasz newsletter!</h1>
+      <p className="text-center mb-3 sm:mb-5 text-md md:text-lg">OPIS NEWSLETTERA w trzech zdaniach, Lorem ipsum dolor sit, amet</p>
+      <form onSubmit={handleFormSubmit} className="flex justify-center mx-3">
+        <div className="md:w-3/6 max-w-xl">
           <input
             ref={emailRef}
             type="email"
             placeholder="e-mail"
-            className="font-medium max-w-full px-5 py-5 mr-2 rounded-md border-2 border-emerald-dark transition-all"
+            className="font-medium px-5 w-full py-5 mr-3 rounded-md border-2 border-emerald-dark transition-all"
           />
         </div>
-        <button className="font-medium px-10 py-5 rounded-md bg-blue-default transition-all text-coolGray-white hover:bg-blue-light" type="submit">
+        <button
+          className="font-medium px-10 py-2 ml-3 md:px-12 rounded-md bg-blue-default transition-all text-coolGray-white hover:bg-blue-light"
+          type="submit"
+        >
           Zapisz się!
         </button>
       </form>
