281e653517791b03a78a8df778e939babc322bc0
TeodorWolski <teodorwolski88@gmail.com>
2021-12-20 14:29:27 +0100

    Changing newsletter section, no styles

--- components/organisms/NewsletterSection/NewsletterSection.tsx
+++ components/organisms/NewsletterSection/NewsletterSection.tsx
@@ -2,19 +2,31 @@ import MailchimpSubscribe, { EmailFormFields } from 'react-mailchimp-subscribe';
 import NewsletterForm from 'components/molecules/NewsletterForm/NewsletterForm';
 import React from 'react';
 
-const NewsletterSubscribe: React.FC<{ withoutDescription?: boolean }> = ({ withoutDescription }) => {
-  const MAILCHIMP_URL = 'https://gmail.us20.list-manage.com/subscribe/post?u=37d42ea39057bd19a6e145ae5&amp;id=fe551fe5a1';
-
+const NewsletterSubscribe = () => {
   return (
-    <MailchimpSubscribe
-      url={MAILCHIMP_URL}
-      render={({ subscribe, status }) => {
-        return (
-          <NewsletterForm withoutDescription={withoutDescription} onValidated={(formData: EmailFormFields) => subscribe(formData)} status={status} />
-        );
-      }}
-    />
+    <a target="_blank" rel="noopener noreferrer" href="https://eepurl.com/hPCZ4f">
+      <button className="font-medium px-10 py-2 ml-3 md:px-12 rounded-md bg-blue-default transition-all text-coolGray-white hover:bg-blue-light">
+        Zapisz się na nasz newsletter!
+      </button>
+    </a>
   );
 };
 
 export default NewsletterSubscribe;
+
+{
+  /* <MailchimpSubscribe
+url={MAILCHIMP_URL}
+render={({ subscribe, status }) => {
+  return (
+    <NewsletterForm withoutDescription={withoutDescription} onValidated={(formData: EmailFormFields) => subscribe(formData)} status={status} />
+  );
+}}
+/>
+);
+
+const MAILCHIMP_URL = 'https://gmail.us20.list-manage.com/subscribe/post?u=37d42ea39057bd19a6e145ae5&amp;id=fe551fe5a1';
+
+
+*/
+}
