2b52af9852b18c8672c98af52f2d6bce80eb8f56
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-22 14:18:51 +0100

    Fix instagram import

--- components/organisms/SocialSection/SocialSection.tsx
+++ components/organisms/SocialSection/SocialSection.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import Instagram from 'public/icons/instagram.svg';
+import Instagram from 'public/icons/Instagram.svg';
 import Medium from 'public/icons/Medium.svg';
 import Image from 'next/image';
 
