642e15830dff12c8bb445e9fab8b2fea28ad7f8a
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-24 14:59:31 +0100

    Add spliting chunks

--- next.config.js
+++ next.config.js
@@ -6,5 +6,9 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ['scontent.fwaw7-1.fna.fbcdn.net', 'avatars.githubusercontent.com']
+  },
+  webpack: (config) => {
+    config.optimization.splitChunks.chunks = 'all';
+    return config;
   }
 });
