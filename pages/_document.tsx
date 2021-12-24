740c92799003b8c72797a3d931bb05459c03a6b2
TeodorWolski <teodorwolski88@gmail.com>
2021-12-24 13:39:17 +0100

    GA configuration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,17 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <link rel="shortcut icon" href="favicon.png" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-YSBMD4QED3"></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YSBMD4QED3');`
+            }}
+          />
         </Head>
         <body>
           <Main />
