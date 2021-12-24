cdf029faa67596d7c8828518e0303d2f933b8e86
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-24 14:51:44 +0100

    Repair next.config.js + add analyze

--- helpers/ga.ts
+++ helpers/ga.ts
@@ -1,9 +1,11 @@
 export const pageView = (url: string) => {
+  // @ts-ignore
   window.gtag('config', 'G-YSBMD4QED3', {
     page_path: url
   });
 };
 
 export const event = ({ action, params }: any) => {
+  // @ts-ignore
   window.gtag('event', action, params);
 };
