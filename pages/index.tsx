e16612e206cd581b00643ec1ce42cac8c1e94a89
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-23 05:22:21 +0100

    Add translation for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,8 +19,7 @@ Home.title = 'Strona Główna';
 export async function getStaticProps({ locale }: any) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common']))
-      // Will be passed to the page component as props
+      ...(await serverSideTranslations(locale, ['home']))
     }
   };
 }
