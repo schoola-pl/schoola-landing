7972af409da89ce5a0c3e0e7b050d35b23652253
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-01-27 18:55:12 +0100

    Add error translation

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ErrorPage from '../components/organisms/ErrorPage/ErrorPage';
 import { useRouter } from 'next/router';
 import { NextPageWithTitle } from '../types/NextPageWithTitle';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
 
 const Err404: NextPageWithTitle = () => {
   const router = useRouter();
+  const { t } = useTranslation('error');
+
   return (
     <ErrorPage
-      title={'Nie możemy tego znaleźć...'}
-      description={'Ale nie martw się, na naszej stronie jest masa innych rzeczy do zwiedzania. Śmiało!'}
-      btnContent={'No i do domku!'}
+      title={t('errors.404.title')}
+      description={t('errors.404.description')}
+      btnContent={t('errors.404.button')}
       func={() => {
         router.push('/');
       }}
@@ -18,5 +22,14 @@ const Err404: NextPageWithTitle = () => {
   );
 };
 Err404.title = '404';
+
+// eslint-disable-next-line
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['error']))
+    }
+  };
+}
 
 export default Err404;
