89b3affcc6fd7114e306950b0d4260446811beaf
JaFe <kontakt.jakubfedoszczak@gmail.com>
2022-03-15 19:47:01 +0100

    Delete translate

--- components/organisms/NewsletterSection/NewsletterSection.tsx
+++ components/organisms/NewsletterSection/NewsletterSection.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { useTranslation } from 'next-i18next';
 
 const NewsletterSubscribe = () => {
-  const { t } = useTranslation('home');
-
   return (
     <div className="flex flex-col justify-center items-center my-10 px-6">
-      <h1 className="text-center font-semibold mb-3 text-xl md:text-2xl">{t('ebook.header')}</h1>
-      <p className="text-center mx-5 mb-3 sm:mb-5 text-md md:text-lg lg:text-xl lg:w-1/2">{t('ebook.text')}</p>
+      <h1 className="text-center font-semibold mb-3 text-xl md:text-2xl">Odbierz darmowego e-booka o 10 metodach skutecznego uczenia się!</h1>
+      <p className="text-center mx-5 mb-3 sm:mb-5 text-md md:text-lg lg:text-xl lg:w-1/2">
+        Newsletter został stworzony z myślą o ambitnych uczniach. Dzięki niemu odbierzesz e-booka o 10 metodach uczenia się i zyskasz porady, które
+        pomogą Ci ukształtować życie zawodowe.
+      </p>
       <a target="_blank" rel="noopener noreferrer" href="https://eepurl.com/hPCZ4f">
         <button className="font-medium my-2 px-16 py-5 ml-3 md:px-12 rounded-md bg-blue-default transition-all text-coolGray-white hover:bg-blue-light">
-          {t('buttons.ebook')}
+          Odbierz e-booka!
         </button>
       </a>
     </div>
--- components/organisms/SocialSection/SocialSection.tsx
+++ components/organisms/SocialSection/SocialSection.tsx
@@ -5,41 +5,41 @@ import Medium from 'public/icons/Medium.svg';
 import Facebook from 'public/icons/FBicon.svg';
 import TikTok from 'public/icons/TikTok.svg';
 import Image from 'next/image';
-import { useTranslation } from 'next-i18next';
 
 const socials = [
   {
     link: 'https://www.instagram.com/schoola__/',
-    description: 'instagram',
+    description:
+      'Instagram to podstawa naszej społeczności. Trzy razy w tygodniu możesz zobaczyć tam posty o tematyce około szkolnej oraz relacje dot. rozwoju.',
     icon: Instagram
   },
   {
     link: 'https://www.medium.com/@schoolapl',
-    description: 'medium',
+    description: 'Jesteś ambitną osobą? Chcesz wiele osiągnąć w życiu? Zapraszamy na naszego bloga, gdzie poruszamy głębiej temat szkoły.',
     icon: Medium
   },
   {
     link: 'https://www.facebook.com/Schoooola',
-    description: 'facebook',
+    description:
+      'Na naszym Facebook, poznasz nas bliżej oraz zobaczysz jakie progresy robimy! Będą też tam udostępniane wpisy dotyczące produktywności oraz skutecznej nauki, sprawdź i zobacz!',
     icon: Facebook
   },
   {
     link: 'https://www.tiktok.com/@schoola__',
-    description: 'tiktok',
+    description:
+      'Na naszym Tik Tok znajdziesz szybkie wskazówki na temat skutecznej nauki oraz technik pamięciowych. Sprawdź czego możesz się nauczyć!',
     icon: TikTok
   }
 ];
 
 const SocialSection = () => {
-  const { t } = useTranslation('home');
-
   return (
     <article>
       <div className="bg-emerald-dark mb-20 py-14">
         <header>
           <div className="flex flex-col items-center justify-center pb-5 px-24">
             <h1 className="font-bold text-white text-3xl text-gray-900 sm:text-4xl lg:text-5xl mb-2 2xl:text-5xl text-center">
-              {t('socials.header')}
+              Śledź nas w internecie!
             </h1>
           </div>
         </header>
@@ -54,7 +54,7 @@ const SocialSection = () => {
                         <Image src={icon} alt="Instagram" />
                       </div>
                     </header>
-                    <p className="px-3 md:px-6 py-4 md:text-lg">{t(`socials.${description}`)}</p>
+                    <p className="px-3 md:px-6 py-4 md:text-lg">{description}</p>
                   </div>
                 </Link>
               </section>
